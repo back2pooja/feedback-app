c1792df7c9de4ec3f7c115d5b1ba923d083b57a4
Pooja Jaiswar <pjaiswar@altimetrik.com>
2024-01-10 15:56:41 +0530

    First commit

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+        Id: 1,
+        rating: 10,
+        text: 'Lorem Example',
+    },
+    {
+        Id: 2,
+        rating: 9,
+        text: 'Lorem Example',
+    },
+    {
+        Id: 3,
+        rating: 8,
+        text: 'Lorem Example',
+    }
+]
+
+export default FeedbackData
